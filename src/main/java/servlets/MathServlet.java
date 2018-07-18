c6377905ba1ffa58ab29b1472391b426eacbfb6e
Natalya Burakova <n-byrakova@mail.ru>
2018-07-18 04:06:34 +0300

    new version of repozitory

--- src/main/java/servlets/MathServlet.java
+++ src/main/java/servlets/MathServlet.java
@@ -9,35 +9,9 @@ import java.io.IOException;
 
 public class MathServlet extends HttpServlet {
     @Override
-    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        String jsonExpr = request.getParameter("expr");
-        Validator validator = new Validator();
-
-        response.setContentType("text/html");
-        response.setCharacterEncoding("UTF-8");
-        PrintWriter writer = response.getWriter();
-
-        String answer;
-        //если json файл является валидным
-        if (validator.isJSONValid(jsonExpr)) {
-            String expr = Analysis.analysis(jsonExpr);//вытаскиваем выражение из json файла
-            if (expr!=null) {
-                SyntaxAnalisator syntaxAnalisator = new SyntaxAnalisator();
-                answer = syntaxAnalisator.getAnalyseStr(expr);
-            }
-            else
-                answer = "{\n" + "\"message\":\"The request must be in the following format: {\"expression\": \"*expression*\" }\"\n" + "}\n";
-        }
-        else {
-            answer = "{\n" + "\"message\":\"Request is not a json object\"\n" + "}\n";
-        }
-
-        writer.print("<!DOCTYPE html>\n" +
-                "<html>\n" +
-                "<p>Answer: </p>\n" +
-                "<body>\n" + answer +
-                "</body>\n" +
-                "</html>");
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        RequestDispatcher requestDispatcher = request.getRequestDispatcher("compute.jsp");
+        requestDispatcher.forward(request, response);
     }
 }
 
